6a4cb702e893c8c3b87d0e77f77b81d0627cf5cc
Aleks-Po <work__address@outlook.com>
2026-09-07 12:06:43 +0100

    07-09-2026

---
+++ block1/07-09-2026/js/script.js
@@ -0,0 +1,18 @@
+// Example 1
+
+/*function updateIntro() {
+	 const intro = document.querySelector("#intro");
+	 intro.textContent = "Updated intro text!";
+}
+
+function changeColorText () {
+	const message = document.querySelector("#message");
+	message.style.color = "#3da86a";
+}
+
+function changeTextFontWeight () {
+	const highlight = document.querySelector(".highlight");
+	highlight.style.fontWeight = "bold";
+}*/
+
+
