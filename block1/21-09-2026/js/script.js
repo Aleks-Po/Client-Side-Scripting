bf6a0f40d5ee6f9e3d0655f5642b8303716129ad
Aleks-Po <work__address@outlook.com>
2026-09-21 12:00:02 +0100

    21-09-2026

---
+++ block1/21-09-2026/js/script.js
@@ -0,0 +1,18 @@
+// function showMessage() {
+// 	const message = document.querySelector("#message");
+// 	message.textContent = "Function is useful!";
+// } 
+
+
+
+
+// function welcomeUser() {
+// 	const userName = document.getElementById("name").value;
+
+// 	if (userName == "") {
+// 		document.getElementById("message").textContent = "Plaese enter your name."
+// 	} else {
+// 		document.getElementById("message").textContent = "Welcome " + userName;
+// 	}
+
+// }
